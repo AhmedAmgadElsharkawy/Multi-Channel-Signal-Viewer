e039b9121e3da7d92ffa320a610581c53d08e8e3
AhmedAmgadEid <ahmedamgad16746@gmail.com>
2024-10-10 17:08:37 +0300

    add multi-channel functionality

--- custom_widgets/rectangle_graph.py
+++ custom_widgets/rectangle_graph.py
@@ -1,7 +1,8 @@
-from PyQt6.QtWidgets import QWidget, QHBoxLayout, QPushButton, QVBoxLayout, QFileDialog
+from PyQt6.QtWidgets import QWidget, QHBoxLayout, QPushButton, QVBoxLayout, QFileDialog,QScrollArea
 import pyqtgraph as pg
 from PyQt6.QtCore import Qt
 import pandas as pd
+from custom_widgets.signal import Signal
 
 
 class RectangleGraph(QWidget):
@@ -9,6 +10,8 @@ class RectangleGraph(QWidget):
         super().__init__()
         self.initUI()
         # self.setStyleSheet("background-color:red")
+        self.signals = []
+        self.xLimit = 4
 
     def initUI(self):
         main_layout = QVBoxLayout()
@@ -23,8 +26,15 @@ class RectangleGraph(QWidget):
         self.rectangle_plot1.setFixedSize(600, 200)
 
         # Create layout for controls and align to the top
+        rectangle_signal_conntrols_widget = QWidget()
         rectangle_plot1_controls = QVBoxLayout()
+        rectangle_signal_conntrols_widget.setLayout(rectangle_plot1_controls)
         rectangle_plot1_controls.setAlignment(Qt.AlignmentFlag.AlignTop)
+        rectangle_signal_conntrols_widget.setFixedSize(100,200)
+
+        self.signals_list1 = QScrollArea()
+        self.signals_list1.setFixedSize(150,200)
+        
 
         # Create buttons
         self.insert_button1 = QPushButton("Insert")
@@ -38,7 +48,9 @@ class RectangleGraph(QWidget):
 
         # Add plot and controls to the container
         rectangle_and_controls_container.addWidget(self.rectangle_plot1)
-        rectangle_and_controls_container.addLayout(rectangle_plot1_controls)
+        rectangle_and_controls_container.addWidget(rectangle_signal_conntrols_widget)
+        rectangle_and_controls_container.addWidget(self.signals_list1)
+
 
         # Add the container to the main layout
         main_layout.addWidget(rectangle_signal_widget)
@@ -47,20 +59,18 @@ class RectangleGraph(QWidget):
         # Connect insert button to file browser
         self.insert_button1.clicked.connect(self.browse_file)
 
-    def load_and_plot_data(self, file_path):
+    def add_signal(self, file_path):
         # Read CSV file
-        data = pd.read_csv(file_path).iloc[:, 1]
-        print(data)
+        if not file_path:
+            pass
 
-        # # Clear any previous plots
-        # self.rectangle_plot1.clear()
-
-        # # Extract the data for plotting
-        # x = data['Time [s]'].values  # Time values
-        # y = data[' V'].values          # V values
-
-        # # Plot the data
-        # self.rectangle_plot1.plot(x, y)  # Blue line with circles
+        signal  = Signal(file_path,len(self.signals))
+        if signal.MaxX > self.xLimit:
+            self.xLimit = signal.MaxX
+        self.signals.append(signal)
+        print(self.signals)
+        self.plot()
+        
 
     def browse_file(self):
         # Open file dialog to select a CSV file
@@ -68,4 +78,9 @@ class RectangleGraph(QWidget):
         file_path, _ = file_dialog.getOpenFileName(self, "Open CSV File", "", "CSV Files (*.csv)")
 
         if file_path:
-            self.load_and_plot_data(file_path)
+            self.add_signal(file_path)
+
+    def plot(self):
+        for signal in self.signals:
+            self.rectangle_plot1.plot(signal.y)
+
---
+++ custom_widgets/signal.py
@@ -0,0 +1,15 @@
+from PyQt6.QtWidgets import QWidget,QLabel
+import pandas as pd
+
+
+class Signal(QWidget):
+    def __init__(self,file_path,index):
+        super().__init__()
+        self.name = f"signal_{index}"
+        data = pd.read_csv(file_path).iloc[:,0:2]
+        self.x = data.iloc[:,0]
+        self.y = data.iloc[:,1]
+        self.MaxX = self.x.iloc[-1]
+
+
+        
