ba2159cb507a4c55eccb2adb3151f2798c08a247
AhmedAmgadEid <ahmedamgad16746@gmail.com>
2024-10-12 17:53:57 +0300

    fix duplicate functions bug

--- main.py
+++ main.py
@@ -29,12 +29,20 @@ class MainWindow(QMainWindow):
         self.rectangle_plot1 = RectangleGraph()
         self.rectangle_plot2 = RectangleGraph()
         main_layout.addWidget(self.rectangle_plot1)
+        self.link_h_widget = QWidget()
         self.link_h_box = QHBoxLayout()
+        self.link_h_widget.setLayout(self.link_h_box)
+        self.line_container = QHBoxLayout()
         self.link_options = QHBoxLayout()
         self.link_button = QCheckBox("Link graphs", self) 
-        self.link_h_box.addWidget(self.link_button)
-        self.link_h_box.addLayout(self.link_options)
-        main_layout.addLayout(self.link_h_box)
+        self.link_h_box.addLayout(self.line_container)
+        self.line_container.addWidget(self.link_button)
+        self.line_container.addLayout(self.link_options)
+        self.link_h_box.addStretch()
+        self.link_h_widget.setStyleSheet("background-color:red")
+        self.line_container.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        
+        main_layout.addWidget(self.link_h_widget)
         main_layout.addWidget(self.rectangle_plot2)
 
         
@@ -42,32 +50,24 @@ class MainWindow(QMainWindow):
 
         glue_widget = QWidget()
         glue_widget_layout = QHBoxLayout()
-        main_layout.addWidget(glue_widget)
         glue_widget.setLayout(glue_widget_layout)
+        self.link_h_box.addWidget(glue_widget)
         self.glue_button = QPushButton("Glue")
         glue_widget_layout.addWidget(self.glue_button)
         self.confirm_glue_button = QPushButton("Confirm")
         self.cancel_glue_button = QPushButton("Cancel")
         self.next_button = QPushButton("Next")
-        # self.choose_glue_signal1 = QComboBox()
-        # self.choose_glue_signal2 = QComboBox()
-        # glue_widget_layout.addWidget(self.choose_glue_signal1)
-        # glue_widget_layout.addWidget(self.choose_glue_signal2)
         glue_widget_layout.addWidget(self.confirm_glue_button)
         glue_widget_layout.addWidget(self.cancel_glue_button)
-        # self.choose_glue_signal1.hide()
-        # self.choose_glue_signal2.hide()
         self.cancel_glue_button.hide()
         self.confirm_glue_button.hide()
         glue_widget_layout.addStretch()
         self.cancel_glue_button.clicked.connect(self.cancel_signals_glue)
         self.glue_button.clicked.connect(self.start_signals_glue)
         self.confirm_glue_button.clicked.connect(self.confirm_signals_glue)
-
         self.glue_button.setEnabled(False)
         self.rectangle_plot1.signals_combobox1.currentIndexChanged.connect(self.update_glue_button)
         self.rectangle_plot2.signals_combobox1.currentIndexChanged.connect(self.update_glue_button)
-
         self.glue_graph = GlueGraph()
         main_layout.addWidget(self.glue_graph)
         
@@ -175,127 +175,6 @@ class MainWindow(QMainWindow):
         
 
 
-
-    def link_button_changed(self):
-        sender = self.sender()
-        if sender.isChecked():
-            if len(self.rectangle_plot1.signals) == 0 or len(self.rectangle_plot2.signals) == 0:
-                QMessageBox.warning(self,"Operation Failed","You can't link the two graphs if one of them is empty")
-                sender.setChecked(False)
-                return
-            self.rectangle_plot1.insert_button1.setEnabled(False)
-            self.rectangle_plot1.play_button1.setEnabled(False)
-            self.rectangle_plot1.pause_button1.setEnabled(False) 
-            self.rectangle_plot1.clear_button1.setEnabled(False) 
-            self.rectangle_plot1.rewind_button1.setEnabled(False)
-            self.rectangle_plot1.speed_up_button1.setEnabled(False)
-            self.rectangle_plot1.speed_down_button1.setEnabled(False)
-            self.rectangle_plot2.insert_button1.setEnabled(False)
-            self.rectangle_plot2.play_button1.setEnabled(False)
-            self.rectangle_plot2.pause_button1.setEnabled(False) 
-            self.rectangle_plot2.clear_button1.setEnabled(False) 
-            self.rectangle_plot2.rewind_button1.setEnabled(False)
-            self.rectangle_plot2.speed_up_button1.setEnabled(False)
-            self.rectangle_plot2.speed_down_button1.setEnabled(False)
-            self.pause_link = QPushButton("Pause", self)
-            self.play_link = QPushButton("Play", self)
-            self.speed_up_link = QPushButton("Speed up", self)
-            self.speed_down_link = QPushButton("Speed down", self)
-            self.rewind_link = QPushButton("Rewind", self)
-            self.link_options.addWidget(self.pause_link)
-            self.link_options.addWidget(self.play_link)
-            self.link_options.addWidget(self.rewind_link)
-            self.link_options.addWidget(self.speed_up_link)
-            self.link_options.addWidget(self.speed_down_link)
-            self.pause_link.clicked.connect(self.pasue_linked_signals)
-            self.play_link.clicked.connect(self.play_linked_signals)
-            self.speed_up_link.clicked.connect(self.speed_up_linked_signals)
-            self.speed_down_link.clicked.connect(self.speed_down_linked_signals)
-            self.rewind_link.clicked.connect(self.rewind_linked_signals)
-            self.rectangle_plot1.rewindSignals()
-            self.rectangle_plot2.rewindSignals()
-            self.rectangle_plot1.rectangle_plot.getViewBox().sigXRangeChanged.connect(self.synchronizePosGraph1)
-            self.rectangle_plot2.rectangle_plot.getViewBox().sigXRangeChanged.connect(self.synchronizePosGraph2)
-            self.rectangle_plot1.rectangle_plot.getViewBox().sigYRangeChanged.connect(self.synchronizePosGraph1)
-            self.rectangle_plot2.rectangle_plot.getViewBox().sigYRangeChanged.connect(self.synchronizePosGraph2)
-        else:
-            self.rectangle_plot1.insert_button1.setEnabled(True)
-            self.rectangle_plot1.play_button1.setEnabled(True)
-            self.rectangle_plot1.pause_button1.setEnabled(True) 
-            self.rectangle_plot1.clear_button1.setEnabled(True) 
-            self.rectangle_plot1.rewind_button1.setEnabled(True)
-            self.rectangle_plot1.speed_up_button1.setEnabled(True)
-            self.rectangle_plot1.speed_down_button1.setEnabled(True)
-            self.rectangle_plot2.insert_button1.setEnabled(True)
-            self.rectangle_plot2.play_button1.setEnabled(True)
-            self.rectangle_plot2.pause_button1.setEnabled(True) 
-            self.rectangle_plot2.clear_button1.setEnabled(True) 
-            self.rectangle_plot2.rewind_button1.setEnabled(True)
-            self.rectangle_plot2.speed_up_button1.setEnabled(True)
-            self.rectangle_plot2.speed_down_button1.setEnabled(True)
-            self.remove_option_widgets()
-
-    def pasue_linked_signals(self):
-        self.rectangle_plot1.pauseSignals()
-        self.rectangle_plot2.pauseSignals()
-    
-    def play_linked_signals(self):
-        self.rectangle_plot1.playSignals()
-        self.rectangle_plot2.playSignals()
-
-    def speed_up_linked_signals(self):
-        self.rectangle_plot1.increaseSpeed()
-        self.rectangle_plot2.increaseSpeed()
-
-
-    def speed_down_linked_signals(self):
-        self.rectangle_plot1.decreaseSpeed()
-        self.rectangle_plot2.decreaseSpeed()
-
-    def rewind_linked_signals(self):
-        self.rectangle_plot1.rewindSignals()
-        self.rectangle_plot2.rewindSignals()
-
-    def remove_option_widgets(self):
-        while self.link_options.count():
-            item = self.link_options.takeAt(0)
-            widget = item.widget()
-            if widget:
-                widget.deleteLater()
-
-    def synchronizePosGraph1(self):
-        if not self.isSyncingX and not self.rectangle_plot1.isRunning:
-            self.rectangle_plot2.rectangle_plot.getViewBox().sigXRangeChanged.disconnect(self.synchronizePosGraph2)
-            self.rectangle_plot2.rectangle_plot.getViewBox().sigYRangeChanged.disconnect(self.synchronizePosGraph2)
-                
-            # Set the X-axis range of graph2 based on graph1
-            xRange = self.rectangle_plot1.rectangle_plot.getViewBox().viewRange()[0]
-            yRange = self.rectangle_plot1.rectangle_plot.getViewBox().viewRange()[1]
-            self.isSyncingX = True
-            self.rectangle_plot2.rectangle_plot.getViewBox().setXRange(*xRange)
-            self.rectangle_plot2.rectangle_plot.getViewBox().setYRange(*yRange)
-            self.isSyncingX = False
-            
-            # Reconnect the signal
-            self.rectangle_plot2.rectangle_plot.getViewBox().sigXRangeChanged.connect(self.synchronizePosGraph2)
-            self.rectangle_plot2.rectangle_plot.getViewBox().sigYRangeChanged.connect(self.synchronizePosGraph2)
-
-    def synchronizePosGraph2(self):
-        if not self.isSyncingX and not self.rectangle_plot2.isRunning:
-            self.rectangle_plot1.rectangle_plot.getViewBox().sigXRangeChanged.disconnect(self.synchronizePosGraph1)
-            self.rectangle_plot1.rectangle_plot.getViewBox().sigYRangeChanged.disconnect(self.synchronizePosGraph1)
-                
-            # Set the X-axis range of graph2 based on graph1
-            xRange = self.rectangle_plot2.rectangle_plot.getViewBox().viewRange()[0]
-            yRange = self.rectangle_plot2.rectangle_plot.getViewBox().viewRange()[1]
-            self.isSyncingX = True
-            self.rectangle_plot1.rectangle_plot.getViewBox().setXRange(*xRange)
-            self.rectangle_plot1.rectangle_plot.getViewBox().setYRange(*yRange)
-            self.isSyncingX = False
-            
-            # Reconnect the signal
-            self.rectangle_plot1.rectangle_plot.getViewBox().sigXRangeChanged.connect(self.synchronizePosGraph1)
-            self.rectangle_plot1.rectangle_plot.getViewBox().sigYRangeChanged.connect(self.synchronizePosGraph1)
 
     def link_button_changed(self):
         sender = self.sender()
